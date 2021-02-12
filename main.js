559d3306b25f1ce1d6eb7eb91433e969be604cc1
Mattis Hovden Aas <mattis@dovremedia.no>
2021-02-12 23:21:20 +0100

    load with https

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.onload = function() {
   // .. and attach the single-file dicom in .NII format
   // this works with gzip/gz/raw encoded NII files but XTK also supports other
   // formats like MGH/MGZ, NRRD and DICOM
-  volume.file = 'http://x.babymri.org/?avf.nii';
+  volume.file = 'https://x.babymri.org/?avf.nii';
 
   r.add(volume);
 
