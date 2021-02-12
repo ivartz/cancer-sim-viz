cec20a4f296f2f3184dec37e84d8403e21aab031
Mattis Hovden Aas <mattis@dovremedia.no>
2021-02-12 23:18:59 +0100

    add onload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // create and initialize a 3D renderer
-var r = new X.renderer3D();
-r.init();
+window.onload = function() {
+  var r = new X.renderer3D();
+  r.init();
 
-// create a X.volume
-var volume = new X.volume();
-// .. and attach the single-file dicom in .NII format
-// this works with gzip/gz/raw encoded NII files but XTK also supports other
-// formats like MGH/MGZ, NRRD and DICOM
-volume.file = 'http://x.babymri.org/?avf.nii';
+  // create a X.volume
+  var volume = new X.volume();
+  // .. and attach the single-file dicom in .NII format
+  // this works with gzip/gz/raw encoded NII files but XTK also supports other
+  // formats like MGH/MGZ, NRRD and DICOM
+  volume.file = 'http://x.babymri.org/?avf.nii';
 
-r.add(volume);
+  r.add(volume);
 
-r.render();
+  r.render();
 
+}
