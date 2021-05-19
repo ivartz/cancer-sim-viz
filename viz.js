dbfe97a9eb1d977d5f563b0e12b75ae5fbe664c7
ivartz <djloek@gmail.com>
2021-05-19 13:29:57 +0000

    untrack data

--- viz.js
+++ viz.js
@@ -8,7 +8,7 @@ window.onload = function() {
 
     // .. associate the TrackVis .TRK file
     //fibers.file = '../data/pathlines-model-enh.trk';
-    fibers.file = 'data/pathlines-model-reduced-enh.trk';
+    fibers.file = 'data/pathlines-reduced.trk';
     
     //fibers.linewidth = 50
 
